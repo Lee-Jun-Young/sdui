89df74fea0a76346a19f10b4a08cdb910a2bf5f9
JunYoung Lee <47595801+Lee-Jun-Young@users.noreply.github.com>
2024-01-26 17:28:31 +0900

    Feat : Add Banner Type List

--- app/src/main/java/com/example/sdui/presentation/main/MainFragment.kt
+++ app/src/main/java/com/example/sdui/presentation/main/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.sdui.presentation.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,8 +11,10 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.example.sdui.data.dto.SectionItemDto
 import com.example.sdui.databinding.FragmentMainBinding
+import com.example.sdui.presentation.toPx
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,6 +25,7 @@ class MainFragment : Fragment() {
     private val viewModel by viewModels<MainViewModel>()
     private lateinit var gridAdapter: MainGridAdapter
     private lateinit var listAdapter: MainListAdapter
+    private lateinit var bannerAdapter: MainViewPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,8 +40,6 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.getItemList()
-        listAdapter = MainListAdapter()
-        gridAdapter = MainGridAdapter()
 
         viewModel.response.observe(viewLifecycleOwner) {
             it.forEach(::drawView)
@@ -47,26 +49,67 @@ class MainFragment : Fragment() {
     private fun drawView(data: SectionItemDto) = with(binding) {
         when (data.viewType) {
             "VIEW_TYPE_CARD" -> {
+                listAdapter = MainListAdapter(data.design)
                 listAdapter.submitList(data.body)
 
-                val rvList = RecyclerView(requireContext())
-                rvList.adapter = listAdapter
-                rvList.layoutManager = LinearLayoutManager(requireContext())
-                fragmentMain.addView(rvList)
-            }
-/*
-            "VIEW_TYPE_BANNER" -> {
-                val rvList = RecyclerView(requireContext())
+                val tvTitle = TextView(requireContext())
+                val params = ViewGroup.MarginLayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                params.setMargins(
+                    toPx(tvTitle.context, data.design?.left ?: 16),
+                    toPx(tvTitle.context, data.design?.top ?: 8),
+                    0,
+                    toPx(tvTitle.context, data.design?.bottom ?: 8)
+                )
+                tvTitle.layoutParams = params
+                tvTitle.text = data.header?.header
+                fragmentMain.addView(tvTitle)
 
+                val rvList = RecyclerView(requireContext())
                 rvList.adapter = listAdapter
                 rvList.layoutManager = LinearLayoutManager(requireContext())
                 fragmentMain.addView(rvList)
             }
- */
+
+            "VIEW_TYPE_BANNER" -> {
+                bannerAdapter = MainViewPagerAdapter()
+                val vpBanner = ViewPager2(requireContext())
+                vpBanner.adapter = bannerAdapter
+
+                vpBanner.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                    override fun onPageSelected(position: Int) {
+                        super.onPageSelected(position)
+                        vpBanner.postDelayed({
+                            if (vpBanner.currentItem < bannerAdapter.itemCount - 1) {
+                                vpBanner.currentItem = vpBanner.currentItem + 1
+                            } else {
+                                vpBanner.currentItem = 0
+                            }
+                        }, 3000)
+                    }
+                })
+                fragmentMain.addView(vpBanner)
+                bannerAdapter.submitList(data.body)
+            }
+
             "VIEW_TYPE_GRID" -> {
+                gridAdapter = MainGridAdapter(data.design)
                 gridAdapter.submitList(data.body)
 
                 val tvTitle = TextView(requireContext())
+                val params = ViewGroup.MarginLayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                params.setMargins(
+                    toPx(tvTitle.context, data.design?.left ?: 16),
+                    toPx(tvTitle.context, data.design?.top ?: 8),
+                    0,
+                    toPx(tvTitle.context, data.design?.bottom ?: 8)
+                )
+                tvTitle.layoutParams = params
                 tvTitle.text = data.header?.header
                 fragmentMain.addView(tvTitle)
 
