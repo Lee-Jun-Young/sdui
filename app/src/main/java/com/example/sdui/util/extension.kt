3797e32a3a2b7523d8790be60dda533c5c0f7483
JunYoung Lee <47595801+Lee-Jun-Young@users.noreply.github.com>
2024-02-03 00:30:25 +0900

    Feat : Add PriceFormat extension

--- app/src/main/java/com/example/sdui/util/extension.kt
+++ app/src/main/java/com/example/sdui/util/extension.kt
@@ -7,3 +7,7 @@ fun toPx(context: Context, dp: Int): Int {
     val density = context.resources.displayMetrics.density
     return (dp.toFloat() * density).roundToInt()
 }
+
+fun toPriceFormat(price: String): String {
+    return price.replace(Regex("(\\d)(?=(\\d{3})+\$)"), "\$1,") + "원"
+}
